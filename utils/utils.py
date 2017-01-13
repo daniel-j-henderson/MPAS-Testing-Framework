25550bcc6887afcaa3e9fd8cc117d177d9c21140
Daniel Henderson <hendersn@ucar.edu>
2017-01-13 11:11:48 -0800

    add in env class constants

--- utils/utils.py
+++ utils/utils.py
@@ -15,9 +15,12 @@ class Result:
 		self.attributes[attname] = value
 		return
 
-ENVLSF = 1
 class Environment:
 
+	NONE = 0
+	ENVLSF = 1
+	ENVPBS = 2
+	
 	def __init__(self):
 		self.params = {}
 
@@ -46,7 +49,7 @@ def runAtmosphereModel(dir, n, env, options={}):
 	exename = 'atmosphere_model'
 
 	# Untested, since YS is down right now
-	if env.get('name') == 'yellowstone' or env.get('type') == ENVLSF:
+	if env.get('name') == 'yellowstone' or env.get('type') == Environment.ENVLSF:
 		print('Running on Yellowstone')
 		args = []
 		for o, l in env.get('lsf_options'):
@@ -64,13 +67,17 @@ def runAtmosphereModel(dir, n, env, options={}):
 		cmd += 'mpirun.lsf ./'+exename
 		print(cmd)
 
-	elif env.get('name') == 'mmm':
+	elif env.get('type') == Environment.ENVPBS:
+		print('Running on '+env.get('name')+', a PBS system.')
+		cmd = ''
+
+	elif env.get('name') == 'mmm' or env.get('type') == Environment.NONE:
 		cmd = 'mpirun -n '+str(n)+' ./'+exename
 		print(cmd)
 
 	err = os.system(cmd)
 	if (err):
-		print('error running mpas in '+dir+', error code '+str(err))
+		print('error running '+exename+' in '+dir+', error code '+str(err))
 		os.chdir(popdir)
 		return False
 	os.chdir(popdir)
@@ -100,7 +107,7 @@ def compareFiles(a, b, env):
 		return False
 		
 
-	f1 = nc.Dataset(a, 'a')
+	f1 = nc.Dataset(a, 'r')
 	f2 = nc.Dataset(b, 'r')
 
 	for k in f1.variables.keys():
