c6b6d3de4b506b3dcc8ee223435b74cfd08c7180
Michael Duda <duda@ucar.edu>
2017-05-10 11:25:18 -0600

    Add tests for static interpolation with both MODIS and USGS land use
    Note: The two new tests, 'static_usgs' and 'static_modis', require
    new files from the Standard Library: x1.40962.grid.nc and WPS_GEOG/.

---
+++ static_usgs/static_usgs.py
@@ -0,0 +1,69 @@
+import os, sys, time
+
+nprocs=1
+compatible_environments = ['kuusi']
+dependencies=['compile_gnu']
+
+def setup(tparams):
+
+	files = ['x1.40962.grid.nc', 'WPS_GEOG']
+	return {'files':files, 'modset':'gnu'}
+
+def test(tparams, res):
+
+	env = tparams['env']
+	utils=env.get('utils')	
+
+	res.set('completed', False)
+	res.set('name', 'Static interpolation (USGS)')
+
+	if not env:
+		print('No environment object passed to Example test, quitting')
+		return res
+	if not utils:
+		print('No utils module in test environment, quitting Example test')
+		return res
+
+	if not env.contains_modset('gnu'):
+		res.set('err_msg', 'In Static/USGS test(), no modset for gnu')
+		return res
+
+	e = env.mod_reset('gnu', {})
+	if e:
+		res.set('err_msg', "In Static/USGS test(), error resetting to the modset 'gnu'.")
+		res.set('err_code', e)
+		return res
+	
+
+	template_dir = tparams['SMARTS_dir']+'/static_usgs'
+	sandbox_dir = tparams['test_dir']
+	exe_dir = tparams['test_dir']+'/../compile_gnu'
+
+	os.system('ln -s '+exe_dir+'/init_atmosphere_model .')
+	utils.linkAllFiles(template_dir+'/inputs', './')
+
+	#(completed, err_code) = runAtmosphereModel(dir, exename, ntasks, env, additional_lsf_options, additional_pbs_options)
+	#completed: boolean, signifies whether the function runAtmosphereModel completed or returned early
+	#err_code: the return code from the model executable
+	#Note: this function blocks until the model run is complete
+
+	#If your test comes with files like namelists and such, link them in this way
+	myRun = utils.modelRun('./', 'init_atmosphere_model', 1, env, add_lsfoptions={'-W':'1:30', '-e':'run.err', '-o':'run.out'})
+	myRun.runModelNonblocking() #returns immediately, model run happening in background
+
+	while not (myRun.is_finished()):
+		time.sleep(1)
+		pass
+
+	myRun.terminate()
+
+	err = myRun.get_result()
+	print('Error code is '+repr(err.get('err_code')))
+
+	res.set('err_code', err.get('err_code'))
+
+	res.set('err_msg', 'Static interplation (USGS) passed')
+
+	res.set('completed', True)
+	
+	return
